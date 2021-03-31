a7c07a9725d04f8d4059dd0277df5f584804c792
wdwiii <wdwhitfield3@gmail.com>
2021-03-31 11:14:16 -0400

    Add function to toggle nav menu

--- js/main.js
+++ js/main.js
@@ -1,13 +1,5 @@
-function rotateBars(x) {
-  x.classList.toggle("change");
-}
-
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function toggleMenu() {
-  var x = document.getElementById("menuitems");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
+$(document).ready(function(){
+    $('.menu-toggle').click(function(){
+        $('nav').toggleClass('active')
+    })
+})
